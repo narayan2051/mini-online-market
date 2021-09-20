762ccaa3ba0520d19494e2793e0c9bd1d9d4ec7e
Sdhungana <dhunganasulab@gmail.com>
2021-09-20 10:46:48 -0500

    added comment

--- src/main/java/com/miu/minionlinemarkert/MiniOnlineMarkertApplication.java
+++ src/main/java/com/miu/minionlinemarkert/MiniOnlineMarkertApplication.java
@@ -8,6 +8,7 @@ public class MiniOnlineMarkertApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiniOnlineMarkertApplication.class, args);
+        //comment
     }
 
 }
